5588b20c79c7e418c184b492c8c300aa0705e6e5
pramesh <pramesh.siet@gmail.com>
2018-01-30 08:44:43 +0530

    Add files via upload

--- Sentimentr.R
+++ Sentimentr.R
@@ -6,18 +6,17 @@
 library(devtools)
 
 #Sentiment Scoring:
-  # Positive Scoring:
-    sentimentr::sentiment_by('i am the happiest man in the world')
+  # Positive Scoring - Aggregated (Averaged) Sentiment Score:
+    sentimentr::sentiment_by('i am the happiest man in the world', by = NULL)
 
-  # Negative Scoring:
-    sentimentr::sentiment_by('i am the most saddest person in the world') 
+  # Negative Scoring - Aggregated (Averaged) Sentiment Score:
+    sentimentr::sentiment_by('i am the most saddest person in the world', by = NULL) 
     
-  # Neutral Scoring:
-    sentimentr::sentiment_by('I am happy to earning more, but not have peaceful life')
+    #sentiment_by will not help when we have multiple sentences with different polarity, hence use Sentence-level scoring with sentiment:
+    sentimentr::sentiment('I am not very happy. He is very happy')
 
 #Extract Sentiment Keywords:
     'My life has become terrible since I met you and lost money' %>% sentimentr::extract_sentiment_terms()
 
 #Sentiment Highlighting:
-    'My life has become terrible since I met you and lost money. But I still have got a little hope left in me' %>% sentimentr::sentiment_by(by = NULL) %>%
-      sentimentr::highlight()
+    'My life has become terrible since I met you and lost money. But I still have got a little hope left in me' %>% sentimentr::sentiment_by(by = NULL) %>% sentimentr::highlight()
